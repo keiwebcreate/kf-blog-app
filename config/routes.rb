c1bf90fac012c05d689ba5a6f23be6b525399f4c
keicreate <keicreate20240704@gmail.com>
2024-12-20 20:02:39 +0900

    記事を変更できるようにしました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
   # get '/' => 'home#index' #ホームコントローラーのインデックスを実行しろって意味
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
